09a2ba0fa690813b7c2b56ec928d23cae16fb2eb
matteobarone <barmat90@gmail.com>
2020-01-14 20:29:35 +0100

    begin local storage set

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {localStorage} from './local-storage/local-storage';
+
 document.addEventListener('DOMContentLoaded', () => {
   const listItemElement = document.querySelector('.notepad__list');
   const inputElement = document.querySelector('.notepad__input');
@@ -16,4 +18,15 @@ function addItem(text, list) {
   el.appendChild(textNode);
   el.classList.add('notepad__item');
   list.appendChild(el);
+
+  localStorage.set('notes', JSON.stringify([text]));
+  console.log('ho aggiunto', text);
 }
+
+// Step per finire il set
+// - init app: get dal local storage dell'array notes (se esiste),
+//   altrimenti settare array vuoto per le nostre notes nella variabile "notesStore"
+// - popolare la variabile "notesStore" con il nuovo dato inserito (notesStore.push(text))
+
+// Step get
+// - popolare la lista delle note con le eventuali note del local storage
