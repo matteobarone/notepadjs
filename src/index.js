3ec708d65aa556c72f26bcc1dfbe997d184d285d
Manuel Alonge <manuel.alonge@gmail.com>
2020-01-21 12:31:29 +0100

    comments

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 });
 
+/*Nota Manuel: ho notato che il local storage memorizza solo l'ultima stringa inserita,
+non tutte come fa il notepad da cui abbiamo preso spunto. Forse c'è un errore. Da vedere insieme.*/
 function addItem(text, list) {
   const el = document.createElement('li');
   const textNode = document.createTextNode(text);
@@ -22,18 +24,24 @@ function addItem(text, list) {
   localStorage.set('notes', JSON.stringify([text]));
   console.log('ho aggiunto', text);
 }
-/*20/01/2020 - Manuel: ho notato che il local storage memorizza solo l'ultima stringa inserita,
-non tutte come fa il notepad da cui abbiamo preso spunto. Forse c'è un errore.*/
 
-// Step per finire il set
-// - init app: get dal local storage dell'array notes (se esiste),
-//   altrimenti settare array vuoto per le nostre notes nella variabile "notesStore"
-// - popolare la variabile "notesStore" con il nuovo dato inserito (notesStore.push(text))
-function getItem(text){  
-  var notesStore = localStorage.getItem('notes');
-  document.getElementById
-  console.log('ho preso' + ' ' + notesStore);
+/* 1. Step per finire il set
+- init app: get dal local storage dell'array notes (se esiste),
+altrimenti settare array vuoto per le nostre notes nella variabile "notesStore"
+- popolare la variabile "notesStore" con il nuovo dato inserito (notesStore.push(text))
+2. Step get
+- popolare la lista delle note con le eventuali note del local storage*/
+
+/*Nota Manuel: ho provato a scrivere il codice sotto, ma in realtà non succede nulla,
+quando però lo faccio da console su Chrome funziona. Magari vediamo insieme stasera*/
+
+if(text.length != 0){
+  localStorage.getItem('notes');
+  console.log(localStorage.getItem('notes'));
+}
+else{
+  var notesStore = new Array();
+  notesStore.push(text);
+  console.log(notesStore);
 }
 
-// Step get
-// - popolare la lista delle note con le eventuali note del local storage
