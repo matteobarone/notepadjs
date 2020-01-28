424c21b118560867e7f21bd9c30e40fae84bad10
Manuel Alonge <manuel.alonge@gmail.com>
2020-01-28 13:10:39 +0100

    commit 28th Jan 13:10

--- src/index.js
+++ src/index.js
@@ -17,24 +17,25 @@ function addItem(text, list) {
   const el = document.createElement('li');
   const textNode = document.createTextNode(text);
   el.appendChild(textNode);
-  el.classList.add('.notepad__item');
+  el.classList.add('notepad__item');
   list.appendChild(el);
-
-  // retrieve the notes that are already in localstorage
-  const notes = JSON.parse(localStorage.get('notes'));
-  // pushing your new note into the notes array - mutation
-  notes.push(text);
+  
   // setting the notes array JSON into localStorage
-  localStorage.set('notes', JSON.stringify(text));   
+  localStorage.set('notes', JSON.stringify(text));
   
   console.log('ho aggiunto', text);
+  // retrieve the notes that are already in localstorage
+  const notes = JSON.parse(localStorage.getItem('notes'));
+  //pushing your new note into the notes array - mutation
+  notes.push(text);
 
 }
 
-function removeItem(text, list){
+// Ho provato con questo per fare il remove Item ma non funziona
+function remove(text, list){
   const removeElement = document.querySelector('.notepad__item::after');
   removeElement.addEventListener('onclick', ()=>{
-    localStorage.remove('notes');
+    JSON.stringify(localStorage.remove(text));
   });
 }
 
